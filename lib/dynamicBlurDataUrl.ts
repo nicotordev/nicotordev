ae49ddb31c4e6f204cc369f9344136c7db5338c9
Nicolas Torres <nicotordev@gmail.com>
2025-06-26 06:21:51 -0400

    ✨ Update dynamicBlurDataUrl to use NEXT_PUBLIC_BASE_URL for improved environment configuration
    🔧 Changes:
    - Replaced `NEXT_PUBLIC_DOMAIN` with `NEXT_PUBLIC_BASE_URL` in the dynamicBlurDataUrl function to ensure consistent environment variable usage.
    
    🌍 This update enhances the configuration for dynamic image URLs, aligning with best practices for environment management.

--- lib/dynamicBlurDataUrl.ts
+++ lib/dynamicBlurDataUrl.ts
@@ -4,7 +4,7 @@ import { assets } from "@/app/assets";
 import { getTranslations } from 'next-intl/server';
 const baseUrl = process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : process.env.NEXT_PUBLIC_DOMAIN || '';
+    : process.env.NEXT_PUBLIC_BASE_URL || '';
 
 export async function dynamicBlurDataUrl(url: string) {
     try {
